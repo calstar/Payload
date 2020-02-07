b504b236126f83ef418cb287df9d539aeb98ac9f
Bryant La <56059488+bryantla@users.noreply.github.com>
2020-02-06 21:59:36 -0800

    fixed x, y, z pins labels

--- Arduino/tests/readAccelerometer/readAccelerometer.ino
+++ Arduino/tests/readAccelerometer/readAccelerometer.ino
@@ -25,24 +25,24 @@
 */
 
 // these constants describe the pins. They won't change:
-const int groundpin = 18;             // analog input pin 4 -- ground
-const int powerpin = 19;              // analog input pin 5 -- voltage
-const int xpin = A3;                  // x-axis of the accelerometer
-const int ypin = A2;                  // y-axis
-const int zpin = A1;                  // z-axis (only on 3-axis models)
+//const int groundpin = 18;             // analog input pin 4 -- ground
+//const int powerpin = 19;              // analog input pin 5 -- voltage
+const int xpin = A19;                  // x-axis of the accelerometer
+const int ypin = A18;                  // y-axis
+const int zpin = A17;                  // z-axis (only on 3-axis models)
 
 void setup() {
   // initialize the serial communications:
   Serial.begin(9600);
 
-  // Provide ground and power by using the analog inputs as normal digital pins.
-  // This makes it possible to directly connect the breakout board to the
-  // Arduino. If you use the normal 5V and GND pins on the Arduino,
-  // you can remove these lines.
-  pinMode(groundpin, OUTPUT);
-  pinMode(powerpin, OUTPUT);
-  digitalWrite(groundpin, LOW);
-  digitalWrite(powerpin, HIGH);
+//  // Provide ground and power by using the analog inputs as normal digital pins.
+//  // This makes it possible to directly connect the breakout board to the
+//  // Arduino. If you use the normal 5V and GND pins on the Arduino,
+//  // you can remove these lines.
+//  pinMode(groundpin, OUTPUT);
+//  pinMode(powerpin, OUTPUT);
+//  digitalWrite(groundpin, LOW);
+//  digitalWrite(powerpin, HIGH);
 }
 
 void loop() {
