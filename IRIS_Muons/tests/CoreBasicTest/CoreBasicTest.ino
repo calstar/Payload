e48eaf570876492dedc3efea93f4428ddc0afe76
Bryant La <bryantla9629@gmail.com>
2021-06-02 23:58:25 -0700

    CoreBasicTest will attempt to print ST IMU data to serial monitor

--- IRIS_Muons/tests/CoreBasicTest/CoreBasicTest.ino
+++ IRIS_Muons/tests/CoreBasicTest/CoreBasicTest.ino
@@ -11,6 +11,16 @@
 #include "ADXL372.h" // Click here to get library: https://github.com/gednrs/ADXL372
 #include <SPI.h>
 
+// ST motion breakout IMU // Click here to get library: https://github.com/gregtomasch/USFSMAX
+#include "Globals.h"
+#include "Alarms.h"
+#include "I2Cdev.h"
+#include "USFSMAX.h"
+#include "Sensor_cal.h"
+#include "IMU.h"
+#include "Types.h"
+#include "def.h"
+
 // Pin 13 has an LED connected on most Arduino boards.
 // Pin 11 has the LED on Teensy 2.0
 // Pin 6  has the LED on Teensy++ 2.0
@@ -32,6 +42,14 @@ int led_y = 32;
 #define ADXL372_DEVID_VAL             0xADu   /* Analog Devices, Inc., accelerometer ID */
 #define ADXL372_RESET                 0x41u   /* Reset */
 #define ADXL372_RESET_CODE            0x52u /* Writing code 0x52 resets the device */
+// ST motion breakout IMU
+#define M_WAKE 15
+#define SCL_1 16
+#define SDA_1 17
+#define M_INT1 24
+#define M_INT2 41
+#define M_INT3 25
+#define M_INT4 40
 
 /* SPI objects (via hardware) */
 ADXL372 myADXL = ADXL372(CS_HG, MOSI_1, MISO_1, SCLK_1);
@@ -40,6 +58,15 @@ ADXL372 myADXL = ADXL372(CS_HG, MOSI_1, MISO_1, SCLK_1);
 // ADXL
 struct ADXL372_AccelTriplet accel;
 struct ADXL372_AccelTripletG accelG;
+// ST IMU
+I2Cdev     i2c_0(&SENSOR_0_WIRE_INSTANCE);
+USFSMAX    USFSMAX_0(&i2c_0, 0);
+IMU        imu_0(&USFSMAX_0, 0);
+Sensor_cal sensor_cal(&i2c_0, &USFSMAX_0, 0);
+void       ProcEventStatus(I2Cdev* i2c_BUS, uint8_t sensorNUM);
+void       FetchUSFSMAX_Data(USFSMAX* usfsmax, IMU* IMu, uint8_t sensorNUM);
+void       DRDY_handler_0();
+void       SerialInterface_handler();
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -79,7 +106,27 @@ void setup() {
   myADXL.Set_ODR(ODR_6400Hz);               // output data rate
   myADXL.Set_hpf_corner(HPF_CORNER0);       // high pass filter cutoff frequency
   myADXL.Set_op_mode(FULL_BW_MEASUREMENT);  // operation mode
-  
+
+  /* ST IMU */
+  // Set up DRDY (magnetometer) interrupt pin
+  pinMode(M_INT1, INPUT);
+  // Initialize USFSMAX_0 I2C bus
+  SENSOR_0_WIRE_INSTANCE.begin(I2C_MASTER, 0x00, SDA_1, I2C_PULLUP_EXT, SCL_1);
+  delay(100);
+  SENSOR_0_WIRE_INSTANCE.setClock(I2C_RATE_100);  // Set I2C clock speed to 100kHz cor configuration
+  delay(2000);
+  // Do I2C bus scan if serial debug is active
+  #ifdef SERIAL_DEBUG                             // Should see MAX32660 slave bus address (default is 0x57)
+    i2c_0.I2Cscan();                                           
+  #endif
+  // Initialize USFSMAX_0
+  #ifdef SERIAL_DEBUG
+    Serial.print("Initializing USFSMAX_0...");
+    Serial.println("");
+  #endif
+  USFSMAX_0.init_USFSMAX();                       // Configure USFSMAX and sensors 
+  SENSOR_0_WIRE_INSTANCE.setClock(I2C_CLOCK);     // Set the I2C clock to high speed for run-mode data collection
+  delay(100);
 }
 
 // the loop routine runs over and over again forever:
@@ -88,6 +135,43 @@ void loop() {
   scan(); //scan for I2C devices and output result over serial (9600 baud)
   //read_ADXL();
   //ADXLscan();
+
+  // updates ST IMU values
+  if(data_ready[0] == 1) {
+    data_ready[0] = 0;
+    ProcEventStatus(&i2c_0, 0);                 // I2C instance 0, Sensor instance 0 (and implicitly USFSMAX instance 0)
+    FetchUSFSMAX_Data(&USFSMAX_0, &imu_0, 0);   // USFSMAX instance 0, IMU calculation instance 0 and Sensor instance 0
+  }
+  // prints ST data to serial monitor
+  Serial.print(" ST Acc (mg) [ ");
+  printFormattedFloat( 1000.0f * accData[0][0], 5, 2 );
+  Serial.print(", ");
+  printFormattedFloat( 1000.0f * accData[0][1], 5, 2 );
+  Serial.print(", ");
+  printFormattedFloat( 1000.0f * accData[0][2], 5, 2 );
+  Serial.print(" ], Gyro (deg/s) [ ");
+  printFormattedFloat( gyroData[0][0], 5, 2 );
+  Serial.print(", ");
+  printFormattedFloat( gyroData[0][1], 5, 2 );
+  Serial.print(", ");
+  printFormattedFloat( gyroData[0][2], 5, 2 );
+  Serial.print(" ], Mag (uT) [ ");
+  printFormattedFloat( magData[0][0], 5, 2 );
+  Serial.print(", ");
+  printFormattedFloat( magData[0][1], 5, 2 );
+  Serial.print(", ");
+  printFormattedFloat( magData[0][2], 5, 2 );
+  Serial.print(" ], Pressure (kPa) [ ");
+  printFormattedFloat( ((float)baroADC[0])*0.1f/4096.0f, 5, 2 );
+  Serial.print(" ], Euler angles: yaw, pitch, roll [ ");
+  printFormattedFloat( heading[0], 5, 2 );
+  Serial.print(", ");
+  printFormattedFloat( angle[0][1], 5, 2 );
+  Serial.print(", ");
+  printFormattedFloat( angle[0][0], 5, 2 );
+  Serial.print(" ]");
+  Serial.println();
+  
   delay(200); 
 }
 
@@ -235,4 +319,223 @@ unsigned int readRegister(byte thisRegister, int bytesToRead) {
   SPI1.endTransaction();
   // return the result:
   return(result);
+}
+
+
+/* Below are complicated utility functions for the ST IMU. */
+
+void ProcEventStatus(I2Cdev* i2c_BUS, uint8_t sensorNUM)
+{
+  uint8_t temp[1];
+
+  // Read algorithm status and event status
+  i2c_BUS->readBytes(MAX32660_SLV_ADDR, COMBO_DRDY_STAT, 1, temp);
+  eventStatus[sensorNUM] = temp[0];
+
+  // Decode the event status to determine what data is ready and set the appropriate DRDY fags
+  if(eventStatus[sensorNUM] & 0x01) Gyro_flag[sensorNUM] = 1;
+  if(eventStatus[sensorNUM] & 0x02) Acc_flag[sensorNUM]  = 1;
+  if(eventStatus[sensorNUM] & 0x04) Mag_flag[sensorNUM]  = 1;
+  if(eventStatus[sensorNUM] & 0x08) Baro_flag[sensorNUM] = 1;
+  if(eventStatus[sensorNUM] & 0x10) Quat_flag[sensorNUM] = 1;
+}
+
+void FetchUSFSMAX_Data(USFSMAX* usfsmax, IMU* IMu, uint8_t sensorNUM)
+{
+  uint8_t call_sensors = eventStatus[sensorNUM] & 0x0F;
+
+  Acq_time = 0;
+  Begin = micros();
+
+  // Optimize the I2C read function with respect to whatever sensor data is ready
+  switch(call_sensors)
+  {
+   case 0x01:
+     usfsmax->GyroAccel_getADC();
+     break;
+   case 0x02:
+     usfsmax->GyroAccel_getADC();
+     break;
+   case 0x03:
+     usfsmax->GyroAccel_getADC();
+     break;
+   case 0x07:
+     usfsmax->GyroAccelMagBaro_getADC();
+     break;
+   case 0x0B:
+     usfsmax->GyroAccelMagBaro_getADC();
+     break;
+   case 0x0F:
+     usfsmax->GyroAccelMagBaro_getADC();
+     break;
+   case 0x0C:
+     usfsmax->MagBaro_getADC();
+     break;
+   case 0x04:
+     usfsmax->MAG_getADC();
+     break;
+   case 0x08:
+     usfsmax->BARO_getADC();
+     break;
+   default:
+     break;
+  };
+  Acq_time += micros() - Begin;
+
+  if(Mag_flag[sensorNUM])
+  {
+    if(ScaledSensorDataFlag)                                                                                         // Calibration data is applied in the coprocessor; just scale
+    {
+      for(uint8_t i=0; i<3; i++)
+      {
+        magData[sensorNUM][i] = ((float)magADC[sensorNUM][i])*UT_per_Count;
+      }
+    } else                                                                                                           // Calibration data applied locally
+    {
+      sensor_cal.apply_adv_calibration(ellipsoid_magcal[sensorNUM], magADC[sensorNUM], UT_per_Count, mag_calData[sensorNUM]);
+      sensor_cal.apply_adv_calibration(final_magcal[sensorNUM], mag_calData[sensorNUM], 1.0f, sensor_point);
+      MAG_ORIENTATION(sensor_point[0], sensor_point[1], sensor_point[2]);
+    }
+    Mag_flag[sensorNUM] = 0;
+  }
+  if(Acc_flag[sensorNUM])
+  {
+    if(ScaledSensorDataFlag)                                                                                         // Calibration data is applied in the coprocessor; just scale
+    {
+      for(uint8_t i=0; i<3; i++)
+      {
+        accData[sensorNUM][i] = ((float)accADC[sensorNUM][i])*g_per_count;
+      } 
+    } else                                                                                                           // Calibration data applied locally
+    {
+      sensor_cal.apply_adv_calibration(accelcal[sensorNUM], accADC[sensorNUM], g_per_count, sensor_point);
+      ACC_ORIENTATION(sensor_point[0], sensor_point[1], sensor_point[2]);
+    }
+    Acc_flag[sensorNUM] = 0;
+  }
+  if(Gyro_flag[sensorNUM] == 1)
+  {
+    if(ScaledSensorDataFlag)                                                                                         // Calibration data is applied in the coprocessor; just scale
+    {
+      for(uint8_t i=0; i<3; i++)
+      {
+        gyroData[sensorNUM][i] = ((float)gyroADC[sensorNUM][i])*dps_per_count;
+      }
+    } else                                                                                                           // Calibration data applied locally
+    {
+      sensor_cal.apply_adv_calibration(gyrocal[sensorNUM], gyroADC[sensorNUM], dps_per_count, sensor_point);
+      GYRO_ORIENTATION(sensor_point[0], sensor_point[1], sensor_point[2]);
+    }
+
+    // Call alternative (Madgwick or Mahony) IMU fusion filter
+    IMu->compute_Alternate_IMU();
+    Gyro_flag[sensorNUM] = 0;
+  }
+  if(Quat_flag[sensorNUM] == 1)
+  {
+    IMu->computeIMU();
+    Quat_flag[sensorNUM] = 0;
+  }
+}
+
+// Host DRDY interrupt handler
+void DRDY_handler_0()
+{
+  data_ready[0] = 1;
+}
+
+// Serial interface handler
+void SerialInterface_handler()
+{
+  serial_input = 0;
+  if(Serial.available()) serial_input = Serial.read();
+  if(serial_input == 49) {sensor_cal.GyroCal();}                                                                     // Type "1" to initiate USFSMAX_0 Gyro Cal
+  if(serial_input == 50)                                                                                             // Type "2" to list current sensor calibration data
+  {
+    SENSOR_0_WIRE_INSTANCE.setClock(I2C_RATE_100);                                                                   // Set I2C clock to 100kHz to read the calibration data from the MAX32660
+    delay(100);
+    USFSMAX_0.Retreive_full_gyrocal();
+    delay(100);
+    USFSMAX_0.Retreive_full_accelcal();
+    delay(100);
+    USFSMAX_0.Retreive_ellip_magcal();
+    delay(100);
+    USFSMAX_0.Retreive_final_magcal();
+    delay(100);
+    SENSOR_0_WIRE_INSTANCE.setClock(I2C_CLOCK);                                                                      // Resume high-speed I2C operation
+    delay(100);
+
+    // Print the calibration results
+    Serial.println("Gyroscope Sensor Offsets (dps)");
+    Serial.println(gyrocal[0].V[0], 4);
+    Serial.println(gyrocal[0].V[1], 4);
+    Serial.println(gyrocal[0].V[2], 4); Serial.println("");
+    Serial.println("Gyroscope Calibration Tensor");
+    Serial.print(gyrocal[0].invW[0][0], 4); Serial.print(",");
+    Serial.print(gyrocal[0].invW[0][1], 4); Serial.print(",");
+    Serial.println(gyrocal[0].invW[0][2], 4);
+    Serial.print(gyrocal[0].invW[1][0], 4); Serial.print(",");
+    Serial.print(gyrocal[0].invW[1][1], 4); Serial.print(",");
+    Serial.println(gyrocal[0].invW[1][2], 4);
+    Serial.print(gyrocal[0].invW[2][0], 4); Serial.print(",");
+    Serial.print(gyrocal[0].invW[2][1], 4); Serial.print(",");
+    Serial.println(gyrocal[0].invW[2][2], 4);
+    Serial.println(""); Serial.println("");
+    Serial.println("Accelerometer Sensor Offsets (g)");
+    Serial.println(accelcal[0].V[0], 4);
+    Serial.println(accelcal[0].V[1], 4);
+    Serial.println(accelcal[0].V[2], 4); Serial.println("");
+    Serial.println("Accelerometer Calibration Tensor");
+    Serial.print(accelcal[0].invW[0][0], 4); Serial.print(",");
+    Serial.print(accelcal[0].invW[0][1], 4); Serial.print(",");
+    Serial.println(accelcal[0].invW[0][2], 4);
+    Serial.print(accelcal[0].invW[1][0], 4); Serial.print(",");
+    Serial.print(accelcal[0].invW[1][1], 4); Serial.print(",");
+    Serial.println(accelcal[0].invW[1][2], 4);
+    Serial.print(accelcal[0].invW[2][0], 4); Serial.print(",");
+    Serial.print(accelcal[0].invW[2][1], 4); Serial.print(",");
+    Serial.println(accelcal[0].invW[2][2], 4);
+    Serial.println(""); Serial.println("");
+    Serial.println("Magnetometer Sensor Offsets (uT)");
+    Serial.println(ellipsoid_magcal[0].V[0], 4);
+    Serial.println(ellipsoid_magcal[0].V[1], 4);
+    Serial.println(ellipsoid_magcal[0].V[2], 4); 
+    Serial.println("");
+    Serial.println("Magnetometer Soft Iron Correction Tensor");
+    Serial.print(ellipsoid_magcal[0].invW[0][0], 4); Serial.print(",");
+    Serial.print(ellipsoid_magcal[0].invW[0][1], 4); Serial.print(",");
+    Serial.println(ellipsoid_magcal[0].invW[0][2], 4);
+    Serial.print(ellipsoid_magcal[0].invW[1][0], 4); Serial.print(",");
+    Serial.print(ellipsoid_magcal[0].invW[1][1], 4); Serial.print(",");
+    Serial.println(ellipsoid_magcal[0].invW[1][2], 4);
+    Serial.print(ellipsoid_magcal[0].invW[2][0], 4); Serial.print(",");
+    Serial.print(ellipsoid_magcal[0].invW[2][1], 4); Serial.print(",");
+    Serial.println(ellipsoid_magcal[0].invW[2][2], 4);
+    Serial.println(""); Serial.println("");
+    Serial.println("Magnetometer Residual Hard Iron Offsets (uT)");
+    Serial.println(final_magcal[0].V[0], 4);
+    Serial.println(final_magcal[0].V[1], 4);
+    Serial.println(final_magcal[0].V[2], 4);
+    Serial.println("");
+    Serial.println("Magnetometer Fine Calibration/Alignment Tensor");
+    Serial.print(final_magcal[0].invW[0][0], 4); Serial.print(",");
+    Serial.print(final_magcal[0].invW[0][1], 4); Serial.print(",");
+    Serial.println(final_magcal[0].invW[0][2], 4);
+    Serial.print(final_magcal[0].invW[1][0], 4); Serial.print(",");
+    Serial.print(final_magcal[0].invW[1][1], 4); Serial.print(",");
+    Serial.println(final_magcal[0].invW[1][2], 4);
+    Serial.print(final_magcal[0].invW[2][0], 4); Serial.print(",");
+    Serial.print(final_magcal[0].invW[2][1], 4); Serial.print(",");
+    Serial.println(final_magcal[0].invW[2][2], 4);
+    Serial.println(""); Serial.println("");
+    sensor_cal.sendOneToProceed();                                                                                   // Halt the serial monitor to let the user read the calibration data
+  }
+  if(serial_input == 51) {USFSMAX_0.Reset_DHI();}                                                                    // Type "3" to reset the DHI corrector
+  serial_input = 0;
+  
+  // Hotkey messaging
+  Serial.println("'1' Gyro Cal");
+  Serial.println("'2' List Cal Data");
+  Serial.println("'3' Reset DHI Corrector");
+  Serial.println("");
 }
